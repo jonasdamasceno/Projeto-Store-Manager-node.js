2665db1c44a19078edc612fb6520a2667867ec96
jdamasceno <jonasmichael2021@outlook.com>
2023-12-03 18:11:27 -0300

    feat: implementa os testes da camada service sales

--- backend/tests/unit/services/sales.service.test.js
+++ backend/tests/unit/services/sales.service.test.js
@@ -1,15 +1,21 @@
-const { expect, use } = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-const connection = require('../../../src/connection/connection');
 const { salesModel } = require('../../../src/models');
-// const { getAllSales } = require('../../../src/models/sales.model');
+const { salesService } = require('../../../src/services');
 const { salesMock, expectedResultById } = require('../../mock/sales.mocks');
 
 describe('testa o endpoint de vendas na camada service', function () {
   afterEach(sinon.restore);
   it('testa a função getAllSalesService', async function () {
-    sinon.stub(connection, 'execute').resolves(salesMock);
-    const responseService = await 
-  });  
+    sinon.stub(salesModel, 'getAllSales').resolves(salesMock);
+    const responseService = await salesService.getAllSalesService();
+    expect(responseService.status).to.equal('SUCCES');
+    expect(responseService.data).to.deep.equal(salesMock);
+  });
+  it('testa o retorno de uma busca feita atraves do id', async function () {
+    sinon.stub(salesModel, 'getSalesById').resolves(expectedResultById);
+    const responseServiceById = await salesService.getSaleByIdService(1);
+    expect(responseServiceById.status).to.equal('SUCCES');
+    expect(responseServiceById.data).to.deep.equal(expectedResultById);
+  });
 });
